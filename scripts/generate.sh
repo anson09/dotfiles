6ff6e4ab35213910d7bb0ca01205449f610d4adc
anson <anson@ricequant.com>
2020-04-08 15:09:45 +0800

    feat: init install shell

--- scripts/generate.sh
+++ scripts/generate.sh
@@ -3,19 +3,11 @@ set -uo pipefail
 
 BAK_PATH=$(cd $(dirname $0)/../config && pwd)
 USER_PATH=/Users/anson
-
 APPLICATIONS=/Applications
 STATICS=($USER_PATH/.vimrc $USER_PATH/.zshrc $USER_PATH/.hyper.js $USER_PATH/.gitconfig /etc/hosts /usr/local/etc/nginx/nginx.conf)
 
-function cpCat {
-    for item in ${STATICS[*]}
-    do
-        cp $item $BAK_PATH
-    done;
-}
-
 echo "STARING 🚀 ..."
-cpCat
+cp ${STATICS[*]} $BAK_PATH
 exa -1 $APPLICATIONS > $BAK_PATH/applications.txt
 brew list | cat > $BAK_PATH/brew.txt
 brew cask list | cat > $BAK_PATH/brew-cask.txt
