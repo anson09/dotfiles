d2007d5262d466eabd2e757539e0d56319ac4dbe
anson <finalsong6@gmail.com>
2021-06-08 18:07:38 +0800

    build: 2021-06-08 18:07:38

--- scripts/generate.sh
+++ scripts/generate.sh
@@ -8,7 +8,7 @@ STATICS=($USER_PATH/.vimrc $USER_PATH/.zshrc $USER_PATH/.gitconfig /etc/hosts /u
 
 echo "STARING 🚀 ..."
 cp ${STATICS[*]} $BAK_PATH
-exa -1 $APPLICATIONS > $BAK_PATH/applications.txt
+ls -1 $APPLICATIONS > $BAK_PATH/applications.txt
 HOMEBREW_NO_AUTO_UPDATE=1 brew bundle dump
 mv Brewfile $BAK_PATH
 ls /Users/anson/.n/lib/node_modules | grep -v npm$ > $BAK_PATH/npm.txt
